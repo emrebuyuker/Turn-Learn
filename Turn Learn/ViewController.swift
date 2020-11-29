99f453cd86744885ed7513916d5a8286ba522c7c
Emre BUYUKER <emre.buyuker@veripark.com>
2020-11-29 14:44:42 +0300

    First commit.

---
+++ Turn Learn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Turn Learn
+//
+//  Created by Emre BÜYÜKER on 29.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
